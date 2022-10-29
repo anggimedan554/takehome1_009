22aaf1a0d51539fb59aff30d51b58bd6b2015cb2
anggihasian111213medan <anggihasiannugraha051001@gmail.com>
2022-10-29 18:07:51 +0700

    create projects

---
+++ src/main/java/pws/c/WarungSayur/WarungSayurApplication.java
@@ -0,0 +1,13 @@
+package pws.c.WarungSayur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarungSayurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarungSayurApplication.class, args);
+	}
+
+}
